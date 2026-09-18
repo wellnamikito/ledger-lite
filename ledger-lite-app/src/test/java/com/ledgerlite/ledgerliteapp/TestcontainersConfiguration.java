5759bc4334dd9f769697e0dce81a5c40f2ad390c
kirillkuklin <Kuklin220706@yandex.ru>
2026-09-18 21:37:02 +0300

    Fix TestcontainersConfiguration: register PostgreSQLContainer bean with @ServiceConnection
    Tests were previously connecting to the local docker-compose Postgres
    instance instead of an isolated Testcontainers instance, since no
    container bean was ever registered.

--- ledger-lite-app/src/test/java/com/ledgerlite/ledgerliteapp/TestcontainersConfiguration.java
+++ ledger-lite-app/src/test/java/com/ledgerlite/ledgerliteapp/TestcontainersConfiguration.java
@@ -1,8 +1,18 @@
 package com.ledgerlite.ledgerliteapp;
 
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
 class TestcontainersConfiguration {
+
+    @Bean
+    @ServiceConnection
+    PostgreSQLContainer<?> postgreSQLContainer(){
+        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:16"));
+    }
 
 }
